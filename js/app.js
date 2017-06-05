7199fe9e1fdae6d3753a091f14c1d86ee16d46ed
tdimnet <thomas.dimnet@gmail.com>
2017-06-05 10:17:11 +0200

    Add new function which display the results

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
+// Fetch the data from the api
 const randomUsersData = fetch('https://randomuser.me/api/?results=10');
 randomUsersData
   .then(data => data.json())
-  .then(data => console.log(data))
+  .then(data => displayResults(data))
+  .catch(err => console.log(err));
+
+
+const displayResults = (data) => {
+  for (let i = 0; i < data.results.length; i++) {
+    let picture = data.results[i].picture.thumbnail;
+    let firstName = data.results[i].name.first;
+    let lastName = data.results[i].name.last;
+    let email = data.results[i].email;
+    let city = data.results[i].location.city;
+    console.log(picture, firstName, lastName, email, city);
+  }
+}
