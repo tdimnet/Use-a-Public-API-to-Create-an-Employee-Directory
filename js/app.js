837ccebd99db8a2deb5a221e60737235209cd364
tdimnet <thomas.dimnet@gmail.com>
2017-06-24 21:56:22 +0200

    Add the over infos for the employee details card

--- js/app.js
+++ js/app.js
@@ -4,10 +4,22 @@ const $overlay = $('<div id="overlay"></div>');
 const $employeeDetail = $('<div id="employee-detail"></div>');
 const $image = $('<img>');
 const $employeeFullname = $('<h2></h2>');
+const $employeeEmail = $('<p class="email"></p>');
+const $employeeCity = $('<p></p>');
+const $horizontalBreak = $('<hr>');
+const $employeePhone = $('<p></p>');
+const $employeeLocation = $('<p></p>');
+const $employeeBirthDay = $('<p></p>');
 
 // Append the elements to the DOM
 $employeeDetail.append($image);
 $employeeDetail.append($employeeFullname);
+$employeeDetail.append($employeeEmail);
+$employeeDetail.append($employeeCity);
+$employeeDetail.append($horizontalBreak);
+$employeeDetail.append($employeePhone);
+$employeeDetail.append($employeeLocation);
+$employeeDetail.append($employeeBirthDay);
 $overlay.append($employeeDetail);
 $('body').append($overlay);
 
@@ -73,11 +85,13 @@ function getOneEmployeeDetails(url) {
     url: url,
     dataType: 'json',
     success: function(data) {
-      console.log(data);
-      const employeeImage = data.results[0].picture.large;
-      const employeeFullname = data.results[0].name.first + ' ' + data.results[0].name.last;
-      $image.attr('src', employeeImage);
-      $employeeFullname.text(employeeFullname);
+      $image.attr('src', data.results[0].picture.large);
+      $employeeFullname.text(data.results[0].name.first + ' ' + data.results[0].name.last);
+      $employeeEmail.text(data.results[0].email);
+      $employeeCity.text(data.results[0].location.city);
+      $employeePhone.text(data.results[0].cell);
+      $employeeLocation.text(data.results[0].location.street + ' ' + data.results[0].location.city + ' ' + data.results[0].location.state + ' ' + data.results[0].location.postcode + ' ' + data.results[0].nat);
+      $employeeBirthDay.text("Birthday: " + data.results[0].dob.substring(0,10));
       $overlay.fadeIn('400');
     }
   });
